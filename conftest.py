df767a94743b730bc739f09816cf356010f4d308
okami <alisovski@mail.ru>
2019-07-13 20:52:53 +0300

    Task 19 Complete

--- conftest.py
+++ conftest.py
@@ -1,20 +1,26 @@
 from fixture.application import Application
 import pytest
 import time
+import json
+import os
+import importlib
+import jsonpickle
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -28,4 +34,20 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook")
+    parser.addoption("--target", action="store", default="target.json")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_form_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_form_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+def load_form_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
--- test/test_add_person.py
+++ test/test_add_person.py
@@ -1,30 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.person import Person
-import time
-import pytest
-import random ,string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join(random.choice(symbols) for i in range(random.randrange(maxlen)))
-
-def random_email():
-    symbols = string.ascii_letters + string.digits
-    username = "".join(random.choice(symbols) for i in range(random.randrange(8)))
-    domainname = "".join(random.choice(symbols) for i in range(random.randrange(8)))
-    return username + "@" + domainname + ".ru"
-
-
-testdata = [
-    Person(fname="", mname="", lname="")] + [
-    Person(fname=random_string("fname", 10), mname=random_string("mname", 20), lname=random_string("lname", 20), email=random_email(),
-           email2=random_email(), email3=random_email())
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("person", testdata, ids=[repr(x) for x in testdata])
-def test_add_person(app, person):
+def test_add_person(app, json_persons):
+    person = json_persons
     old_persons = app.person.get_person_list()
     app.person.create(person)
     new_persons = app.person.get_person_list()
